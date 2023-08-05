6e0ef0305cf959046a641b2248a26b719d85d557
pumbas600 <joshjeffers600@gmail.com>
2023-08-05 16:52:51 +1200

    feat: :sparkles: mock get by id book responses with 1984 book content

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -6,6 +6,8 @@ import { isValidObjectId } from 'mongoose'
 
 export const bookRoute = Router()
 
+const BookId1984 = '64cdd10923b0d36c39921d23'
+
 bookRoute.route('/').get(async (req: Request, res: Response) => {
   try {
     const bookMetadata = await BookModel.find().select(['title', 'coverImage', 'author', 'publishDate'])
@@ -28,9 +30,13 @@ bookRoute.route('/:bookId').get(async (req: Request<{ bookId: string }>, res: Re
       return res.status(Status.NotFound).json(`There is no book with the id '${req.params.bookId}'`)
     }
 
+    // TODO: Remove we don't have content for the other books yet
+    const book1984 = await BookModel.findById(BookId1984)
+    const mockedBook = { ...book, content: book1984.content }
+
     // TODO: Only return first few pages
 
-    return res.status(Status.Ok).json(book)
+    return res.status(Status.Ok).json(mockedBook)
   } catch (err) {
     return ErrorService.handleError(res, err)
   }
