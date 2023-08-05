723c2c0e062cdebaeaf458c7ec9a7590965c6de1
pumbas600 <joshjeffers600@gmail.com>
2023-08-05 15:36:17 +1200

    feat: :sparkles: Add get book by id endpoint

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -3,11 +3,27 @@ import { BookModel } from '../models/book'
 
 export const bookRoute = Router()
 
-bookRoute.route('/api/books').get(async (req: Request, res: Response) => {
+bookRoute.route('/').get(async (req: Request, res: Response) => {
   try {
     const bookMetadata = await BookModel.find().select(['title', 'coverImage', 'author', 'publishDate'])
 
     return res.status(200).json(bookMetadata)
+  } catch (err) {
+    return res.status(500).json(err.message)
+  }
+})
+
+bookRoute.route('/:bookId').get(async (req: Request<{ bookId: string }>, res: Response) => {
+  try {
+    const book = await BookModel.findById(req.params.bookId)
+
+    if (!book) {
+      return res.status(404).json(`There is no book with the id ${req.params.bookId}`)
+    }
+
+    // TODO: Only return first few pages
+
+    return res.status(200).json(book)
   } catch (err) {
     return res.status(500).json(err.message)
   }
