1d0bee1e8834db988f2be7b0c521d2c1823d7ef4
pumbas600 <joshjeffers600@gmail.com>
2023-08-05 12:40:43 +1200

    feat: :sparkles: Create book interface

---
+++ src/types/book.ts
@@ -0,0 +1,9 @@
+export interface BookType {
+    title: string;
+    coverImage: string;
+    author: string;
+    publishDate: Date;
+    blurb: string;
+    originalContent: string;
+    translatedContent: Record<string, string>;
+}
